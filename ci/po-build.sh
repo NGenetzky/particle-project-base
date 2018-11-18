8c4bc4bb02f10f0f29f2a8e3ff9fd1901c438eaa
Nathan Genetzky <nathan@genetzky.us>
2018-11-17 22:04:50 -0600

    ci: Split into travis.sh into po-install.sh and po-build.sh

---
+++ ci/po-build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# po lib clean . -f &> /dev/null
+# yes "no" | po lib setup # change to "yes" to prefer libraries from GitHub
+po photon build
---
+++ ci/po-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt install -y expect
+
+RAW_GITHUB_PO='https://raw.githubusercontent.com/nrobinson2000/po/175597cc588e0cf8e4193feed9354fb447520766/'
+sudo curl -fsSLo /usr/local/share/po-common "${RAW_GITHUB_PO}/share/po-common"
+sudo curl -fsSLo /usr/local/share/po-linux "${RAW_GITHUB_PO}/share/po-linux"
+sudo curl -fsSLo /usr/local/bin/po "${RAW_GITHUB_PO}/bin/po"
+sudo chmod +x /usr/local/bin/po
+
+po config release/v0.6.3 duo false
+
+po install "${HOME}/.po-util/src" basic
