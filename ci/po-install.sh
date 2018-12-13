be66f0b16f9514c5394c7339a5ea99339fa4c168
Nathan Genetzky <nathan@genetzky.us>
2018-12-12 23:17:12 -0600

    ci: Integrate gitlab_pre_build into po-install.sh

--- ci/po-install.sh
+++ ci/po-install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-sudo apt-get update
 
-sudo apt install -y expect
-
+SRCREV_po="26d1080e79c758839872fe83170b0cf363c5f34f"
 RAW_GITHUB_PO='https://raw.githubusercontent.com/nrobinson2000/po/175597cc588e0cf8e4193feed9354fb447520766/'
-sudo curl -fsSLo /usr/local/share/po-common "${RAW_GITHUB_PO}/share/po-common"
-sudo curl -fsSLo /usr/local/share/po-linux "${RAW_GITHUB_PO}/share/po-linux"
-sudo curl -fsSLo /usr/local/bin/po "${RAW_GITHUB_PO}/bin/po"
-sudo chmod +x /usr/local/bin/po
+
+apt-get update
+apt install -y \
+  expect \
+  sudo
+
+sudo curl -fsSLo '/usr/local/share/po-common' "${RAW_GITHUB_PO}/share/po-common"
+sudo curl -fsSLo '/usr/local/share/po-linux' "${RAW_GITHUB_PO}/share/po-linux"
+sudo curl -fsSLo '/usr/local/share/po-docker' "${RAW_GITHUB_PO}/share/po-docker"
+sudo curl -fsSLo '/usr/local/bin/po' "${RAW_GITHUB_PO}/bin/po"
+sudo chmod +x '/usr/local/bin/po'
 
 if [ -f "./ci/po-config.bash" ]; then
   install -m 644 \
